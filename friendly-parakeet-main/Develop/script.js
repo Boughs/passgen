ccb3a51e2d4d0584ef987476a97ce19907bee8d0
Boughs <mattvillagran@gmail.com>
2021-12-19 17:51:50 -0600

    attempting to get generated password into textbox

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,30 +1,60 @@
 // Assignment code here
 
-
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-//Generate Password fucntion
-function generatePassword() {
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "1234567890";
-  var special = "`~!@#$%^&*()_-+={}[];:'<>,.?/";
+  var special = "!@#$%^&*_-+=";
+//Generate Password fucntion
+function generatePassword() {
   var password = "";
   var passwordChar = "";
 
-  var length = prompt("Choose a password length from 8-128 characters!");
-  length = parseInt(length);
 
-  if (length < 8) {
+  var userLength = prompt("Choose a password length from 8-128 characters!");
+  userLength = parseInt(userLength);
+
+  if (userLength < 8) {
     alert("Passwords must be at or above 8 characters!");
     return "";
   }
 
-  if (length > 128) {
+  if (userLength > 128) {
     alert("Passwords must not have more than 128 characters!");
-    return "";
+    return ""
   }
+
+  var useUpper = confirm("Include uppercase characters in your password?");
+
+  if (useUpper) {
+    passwordChar += uppercase;
+  }
+
+  var useLower = confirm("Include lowercase characters in your password?");
+
+  if (useLower) {
+    passwordChar += lowercase;
+  }
+
+  var useNumber = confirm("Include numbers in your password?");
+
+  if (useNumber) {
+    passwordChar += numbers;
+  }
+
+  var useSpecial = confirm("Include special characters in your password?");
+
+  if (useSpecial) {
+    passwordChar += special
+  }
+
+  for (var i = 0; i < length; i++) {
+    password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
+  }
+
+  
   // 1. Promt user for the password criteria
   // a. password length between 8 and 128 characters
   // b. lowercase, uppercase, numbers, special characters
@@ -32,8 +62,8 @@ function generatePassword() {
   // 3. Generate password based on criteria
 
   // 4. Display the generated password on the page
-}
 
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -41,7 +71,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
