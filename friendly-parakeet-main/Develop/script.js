b671c2545161c0be5de2c4977494da6ce7efbe90
Boughs <mattvillagran@gmail.com>
2021-12-19 23:06:11 -0600

    password attribute prompts and varifications

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 // Function that will create users password to their specifications
 function generatePassword(){
   // characteristics for our password along with the final password and our work in progress password variables
-  var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var digits = "1234567890";
   var special = "~!@#$%^&*()_+{}:?><;.,";
@@ -24,6 +24,34 @@ function generatePassword(){
     alert("PASSWORD MUST BE GREATER THAN 8 OR LESS THAN 128 CHARACTERS PLEASE TRY AGAIN!!!!")
     return "";
   }
+
+  // user prompts for what to include in password
+  var wantCapital = confirm("Would you like capital letters in your password?")
+  var wantLowercase = confirm("Would you like lowercase letters in your password?")
+  var wantDigits = confirm("Would you like numbers in your password?")
+  var wantSpecial = confirm("Would you like special characters in your password?")
+
+  // if atleast one prompt was selected a random character will be assigned to desired password length
+  if (wantCapital) {
+    passwordInProgress += capital
+  }
+  if (wantLowercase) {
+    passwordInProgress += lowercase
+  }
+  if (wantDigits) {
+    passwordInProgress += digits
+  }
+  if (wantSpecial) {
+    passwordInProgress += special
+  }
+
+  // When no character types are selected user will be promted to at least pick one attribute
+  if (
+    !wantCapital && !wantLowercase && !wantDigits && !wantSpecial
+  ) {
+    return alert("One attribute must be chosen for password generation!!!");
+  }
+  
 }
 // Write password to the #password input
 function writePassword() {
