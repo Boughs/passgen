a2d50c2e4f426dd7ae7aec1c9d72852fbcb0da24
Boughs <mattvillagran@gmail.com>
2021-12-19 22:52:17 -0600

    set password length prompt and verify statement

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -13,6 +13,17 @@ function generatePassword(){
   var special = "~!@#$%^&*()_+{}:?><;.,";
   var password = "";
   var passwordInProgress = "";
+
+  // asking user password length
+  var longPass = prompt("Choose a password length between 8 and 128 characters!")
+  // check to see if password matches length parameters
+  if (longPass >= 8 && longPass <= 128) {
+  }
+  // if password is in fact less than 8 or greater than 128 user will be prompted to re enter length
+  else {
+    alert("PASSWORD MUST BE GREATER THAN 8 OR LESS THAN 128 CHARACTERS PLEASE TRY AGAIN!!!!")
+    return "";
+  }
 }
 // Write password to the #password input
 function writePassword() {
